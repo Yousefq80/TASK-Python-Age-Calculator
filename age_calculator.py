d323959d6d1e95eaaa5bc73a5bd9348a0ef6f26f
Yousefq80 <ysalharby@gmail.com>
2022-10-02 01:25:11 +0300

    final answer

--- age_calculator.py
+++ age_calculator.py
@@ -1,19 +1,50 @@
+from calendar import month
 from datetime import date
 
 
-def get_dob():
-    # write code here
-	...
 
+
+def get_dob():
+	  
+    y = int(input("please enter your birth year: "))
+    m = int(input("please enter your birth month: "))
+    d = int(input("please enter your birth day: "))
+    dob = date(y, m, d)
+    return dob
+    
+    # # write code here
+
+	# global dob
+
+	# yr,mont,dy= map(int,input("Enter year ,month ,day separated by spaces = ").split())
+	# dob=date(yr,mont,dy)
+	# print(dob)
+	
+	
+    # da=datetime.(y,m,day)
 
 def get_age(dob):
-    # write code here
-	...
 
+	curentdate=date.today()
+	age= (curentdate - dob)
+
+	# print(age)
+	return age
+    # write code here
+	
+	
+    
+    # return age_in_days 
 
 def main():
-	# write code here
-	...
+	
+    dob = get_dob()
+    t = date.today()
+    age = int((t - dob).days / 365)
+    if dob < t:
+        print(f"Your age is:,{age}")
+    else:
+        print("error")
 
 
 if __name__ == '__main__':
